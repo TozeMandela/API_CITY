21abd4a3ca6fa58f763d4d855b28dedc72204bd1
dev-mandela <mandelajunior10@gmail.com>
2024-03-09 12:48:45 +0100

    update server for dont't migrate tables automatic in development

--- src/Server/server.ts
+++ src/Server/server.ts
@@ -15,8 +15,20 @@ app.on('on', () => {
 
 });
 
-Knex.migrate.latest().then(()=> {
-	app.emit('on');
-}).catch(e => {
-	console.log('Erro ao se connectar com a base de dados', e);
-});
+if(process.env.IN_DEVELOPMENT === 'true'){
+	Knex.migrate.latest().then(()=> {
+		console.log('database on');
+
+		app.emit('on');
+	}).catch(e => {
+		console.log('Erro ao se connectar com a base de dados', e);
+	});
+} else {
+	app.listen(process.env.PORT || PORT, () => {
+		console.log('====================================');
+		console.log('server running in port ', PORT);
+		console.log('====================================');
+	});
+}
+
+
