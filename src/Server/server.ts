cbee26dd9d69dfeedd9f96fbef7234ce0e9c9723
dev-mandela <mandelajunior10@gmail.com>
2024-03-09 12:27:22 +0100

    edit server

--- src/Server/server.ts
+++ src/Server/server.ts
@@ -1,10 +1,22 @@
 import 'dotenv/config';
 import { app } from '../app';
+import { Knex } from '../db/knex';
 
 const PORT = 3330;
 
-app.listen(process.env.PORT || PORT, () => {
-	console.log('====================================');
-	console.log('server running in port ', PORT);
-	console.log('====================================');
+
+app.on('on', () => {
+
+	app.listen(process.env.PORT || PORT, () => {
+		console.log('====================================');
+		console.log('server running in port ', PORT);
+		console.log('====================================');
+	});
+
+});
+
+Knex.migrate.latest().then(()=> {
+	app.emit('on');
+}).catch(e => {
+	console.log('Erro ao se connectar com a base de dados', e);
 });
